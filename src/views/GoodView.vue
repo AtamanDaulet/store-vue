c5f9a8e2ad2ce45e99346cf6e070fa459a5346bf
Daulet Atambayev <atamandaulet@gmail.com>
2023-12-15 16:41:31 -0600

    goodview base

--- src/views/GoodView.vue
+++ src/views/GoodView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
+  <div class="flex justify-center gap-2 mx-4 my-2 h-96">
+    <div class="mx-2 w-2/3 ">
+      <div class="text-2xl font-bold pl-2">HP</div>
+      <div class="flex h-full gap-2">
+        <div class="w-2/3 bg-slate-400 ">Images</div>
+        <div class="w-1/2 bg-slate-300">Description 
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, tempora?
+        </div>
+      </div>
 
+    </div>
+    <div class="mx-2  w-1/3 flex flex-col justify-evenly">
+      <div class="text-2xl text-center">1200 $</div>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >Add to basket</button>
+    </div>
+    
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
