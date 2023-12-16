35e9291b031acf9ca93ed28b13c3ca6d8f997a74
Daulet Atambayev <atamandaulet@gmail.com>
2023-12-15 23:03:21 -0600

    cardview done

--- src/service/goods.service.ts
+++ src/service/goods.service.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import type { Good } from '@/types/goods';
 
-const url  = 'http://localhost:3000';
+const url = 'http://localhost:3000';
 
 export const useGoodsService = () => {
   const convertToGood = (obj: any): Good => {
     return {
       id: obj._id,
       name: obj.name,
-      price: obj.price,
+      price: obj.price
     };
   };
 
@@ -16,24 +16,25 @@ export const useGoodsService = () => {
     const result = await axios.get(`${url}/goods`);
     return result.data.map((obj: any) => convertToGood(obj));
   };
-  const getGood = async (id: string): Promise<Good> => {
+  const getGood = async (id: string): Promise<Good | null> => {
     const result = await axios.get(`${url}/goods/get/${id}`);
+    if (!result) return null;
     return convertToGood(result.data);
   };
-  const postGood = async (name:string, price:number): Promise<string | Error> => {
+  const postGood = async (name: string, price: number): Promise<string | Error> => {
     try {
-      return await axios.post(`${url}/goods/add`, {name, price});      
+      return await axios.post(`${url}/goods/add`, { name, price });
     } catch (error) {
       return error as Error;
     }
   };
-  const deleteGood = async (id:string): Promise<string | Error> => {
+  const deleteGood = async (id: string): Promise<string | Error> => {
     try {
-      return await axios.delete(`${url}/goods/delete/${id}`);      
+      return await axios.delete(`${url}/goods/delete/${id}`);
     } catch (error) {
       return error as Error;
     }
   };
 
-  return { getAllGoods, getGood, postGood , deleteGood};
+  return { getAllGoods, getGood, postGood, deleteGood };
 };
--- src/views/GoodView.vue
+++ src/views/GoodView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-center gap-2 mx-4 my-2 h-96">
+  <section class="flex justify-center gap-2 mx-4 my-2 h-96">
     <div class="mx-2 w-2/3 ">
-      <div class="text-2xl font-bold pl-2">HP</div>
+      <div class="text-2xl font-bold pl-2">{{ good?.name }}</div>
       <div class="flex h-full gap-2">
         <div class="w-2/3 bg-slate-400 ">Images</div>
         <div class="w-1/2 bg-slate-300">Description 
@@ -11,15 +11,33 @@
 
     </div>
     <div class="mx-2  w-1/3 flex flex-col justify-evenly">
-      <div class="text-2xl text-center">1200 $</div>
+      <div class="text-2xl text-center">{{ good?.price }} $</div>
       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >Add to basket</button>
     </div>
     
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import type { Good } from '@/types/goods';
+import { useGoodsService } from '@/service/goods.service';
+import { ref, onMounted } from 'vue';
+
+const props = defineProps<{
+  id:  Good["id"];}
+  >();
+
+const {getGood} = useGoodsService();
+
+const good = ref<Good| null>(null);
+ 
+onMounted(async () => {
+  good.value = await getGood(props.id);
+});
+
+  
+
 </script>
 
 <style scoped>
